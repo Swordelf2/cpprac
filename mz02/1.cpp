6796abe2c0c789685cd6a036cd9291a47f82e234
Swordelf <Swordelf@mail.ru>
2018-02-26 11:36:37 +0300

    n/a

---
+++ mz02/1.cpp
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+
+namespace numbers {
+
+class complex
+{
+    public:
+    complex();
+    complex(const double &a, double _b = 0.0);
+    explicit complex(const char *str);
+    double get_re() const;
+    double get_im() const;
+    double abs2() const;
+    double abs() const;
+    void to_string(char *buf, size_t size) const;
+    friend const complex operator+(complex c1, complex c2);
+    const complex operator-(complex other) const;
+    const complex operator*(complex other) const;
+    const complex operator/(complex other) const;
+
+    friend const complex operator+(double d, complex c);
+    friend const complex operator-(double d, complex c);
+    friend const complex operator*(double d, complex c);
+    friend const complex operator/(double d, complex c);
+
+    const complex operator~() const;
+    const complex operator-() const;
+
+    private:
+    double a;
+    double b;
+};
+
+complex::complex()
+{
+    a = b = 0.0;
+}
+
+complex::complex(const double &_a, double _b)
+{
+    a = _a;
+    b = _b;
+}
+
+complex::complex(const char *str)
+{
+    sscanf(str, "(%lf, %lf)", &a, &b);
+}
+
+double complex::get_re() const
+{
+    return a;
+}
+
+double complex::get_im() const
+{
+    return b;
+}
+
+double complex::abs2() const
+{
+    return a * a + b * b;
+}
+
+double complex::abs() const
+{
+    return sqrt(abs2());
+}
+
+void complex::to_string(char *buf, size_t size) const
+{
+    snprintf(buf, size, "(%.10g,%.10g)", a, b);
+}
+
+const complex operator+(complex c1, complex c2)
+{
+    double resa = c1.get_re() + c2.get_re();
+    double resb = c1.get_im() + c2.get_im();
+    complex res(resa, resb);
+    return res;
+}
+
+const complex complex::operator-(complex other) const
+{
+    double resa = a - other.get_re();
+    double resb = b - other.get_im();
+    complex res(resa, resb);
+    return res;
+}
+
+const complex complex::operator*(complex other) const
+{
+    double resa = a * other.get_re() - b * other.get_im();
+    double resb = a * other.get_im() + b * other.get_re();
+    complex res(resa, resb);
+    return res;
+}
+
+const complex complex::operator/(complex other) const
+{
+    double a1, a2, b1, b2;
+    a1 = a;
+    a2 = other.a;
+    b1 = b;
+    b2 = other.b;
+    double resa = (a1 * a2 + b1 * b2) / (a2 * a2 + b2 * b2);
+    double resb = (a2 * b1 - a1 * b2) / (a2 * a2 + b2 * b2);
+    complex res(resa, resb);
+    return res;
+}
+
+
+const complex operator+(double d, complex c)
+{
+    return c + d;
+}
+
+const complex operator-(double d, complex c)
+{
+    return c - d;
+}
+
+const complex operator*(double d, complex c)
+{
+    return c * d;
+}
+
+const complex operator/(double d, complex c)
+{
+    complex c0(d, 0);
+    return c0 / c;
+}
+
+const complex complex::operator~() const
+{
+    double resa = a;
+    double resb = -b;
+    complex res(resa, resb);
+    return res;
+}
+
+const complex complex::operator-() const
+{
+    double resa = -a;
+    double resb = -b;
+    complex res(resa, resb);
+    return res;
+}
+
+}
+
+/*
+using namespace numbers;
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    complex c1(a, b);
+    char buf[256];
+    (c1 + 2.0).to_string(buf, sizeof(buf));
+    cout << buf << endl;
+}
+*/
