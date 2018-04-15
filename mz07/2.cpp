399d76ffa735ef6b6a571131763499aa4d6586e6
Evgeny <Swordelf@mail.ru>
2018-04-15 15:28:35 +0300

    n/a

--- mz07/2.cpp
+++ mz07/2.cpp
@@ -3,12 +3,23 @@
 
 void process(std::vector<int> &v)
 {
-    for (auto it = v.begin(); it != v.end(); ++it) {
-        it = v.erase(it);
-        if (it == v.end()) {
-            break;
+    if (v.empty()) {
+        return;
+    }
+
+    auto it = v.begin() + 1;
+    size_t cnt = 0;
+    while (it < v.end()) {
+        *(v.begin() + (it - v.begin() - 1) / 2) = std::move(*it);
+        ++cnt;
+        if (it < v.end() - 2) {
+            it += 2;
+        } else {
+            it = v.end();
         }
     }
+    v.resize(cnt);
+
     for (auto it = v.rbegin(); it != v.rend(); ++it) {
         std::cout << *it << ' ';
     }
